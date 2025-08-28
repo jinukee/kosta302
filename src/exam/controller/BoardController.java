30e9e8b98db46dad109e8ed3de8965bc0444e4f8
jinukee <jinukee333@gmail.com>
2025-08-28 15:43:50 +0900

    충돌 해결!

--- src/exam/controller/BoardController.java
+++ src/exam/controller/BoardController.java
@@ -10,7 +10,12 @@ public class BoardController {
 		System.out.println("서버에서 작업함~");
 	}
 
+
 	public void delete() {
 		System.out.println("원격에서 삭제 추가~");
+	}
+	
+	public void update() {
+		System.out.println("로컬에서 update 입니다.~");
 	}
 }
