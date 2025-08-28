776ce6eadc2e7ded7cf7bc8a989a2353e4f8dec0
jinukee <87477498+jinukee@users.noreply.github.com>
2025-08-28 16:54:30 +0900

    원격에서 Update MainView.java

--- src/exam/view/MainView.java
+++ src/exam/view/MainView.java
@@ -4,5 +4,6 @@ public class MainView {
 	public static void main(String[] args) {
 		System.out.println("시작합니다~~");
 		System.out.println("I'm hungry");
+		System.out.println("pull request testing");
 	}
 }
